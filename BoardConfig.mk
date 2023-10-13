c29461b0790679cff3918f61ec5613a6932bde70
luka177 <lukapanio@gmail.com>
2023-10-13 21:51:56 +0200

    treewide: alioth -> pipa
    Change-Id: I78e5f5f86b8c21e1990fca04a7a6ba8939863051

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -7,7 +7,7 @@
 # Inherit from sm8250-common
 include device/xiaomi/sm8250-common/BoardConfigCommon.mk
 
-DEVICE_PATH := device/xiaomi/alioth
+DEVICE_PATH := device/xiaomi/pipa
 
 BUILD_BROKEN_DUP_RULES := true
 
@@ -22,13 +22,10 @@ TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):init_xiaomi_alioth
 TARGET_RECOVERY_DEVICE_MODULES := init_xiaomi_alioth
 
 # Kernel
-TARGET_KERNEL_CONFIG += vendor/xiaomi/alioth.config
-
-# OTA assert
-TARGET_OTA_ASSERT_DEVICE := alioth,aliothin
+TARGET_KERNEL_CONFIG += vendor/xiaomi/pipa.config
 
 # Properties
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
 # Inherit from the proprietary version
-include vendor/xiaomi/alioth/BoardConfigVendor.mk
+include vendor/xiaomi/pipa/BoardConfigVendor.mk
