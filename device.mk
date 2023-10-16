00d0abebe6f7662de7fe60c0964b9ce8ae25a818
Davide Garberi <dade.garberi@gmail.com>
2023-10-16 02:09:53 +0200

    pipa: Specify that the device is a tablet
     * This is needed for differentiation in the common tree
    
    Change-Id: I6e41aaf98bea7670b10302983a78e4dfd3f74ef7

--- device.mk
+++ device.mk
@@ -4,8 +4,8 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# A/B
 TARGET_IS_VAB := true
+TARGET_IS_TABLET := true
 
 # Inherit from sm8250-common
 $(call inherit-product, device/xiaomi/sm8250-common/kona.mk)
