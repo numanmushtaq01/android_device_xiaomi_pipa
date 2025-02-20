8d5fc2997f06e7ab37a9e89f2fc4524e82aa9b98
Abdulwahab Isam <abdoi94.iq@gmail.com>
2025-02-20 17:08:57 +0300

    pipa: lets do axion
    Signed-off-by: Abdulwahab Isam <abdoi94.iq@gmail.com>

--- lineage_pipa.mk
+++ lineage_pipa.mk
@@ -28,5 +28,10 @@ TARGET_SUPPORTS_QUICK_TAP := false
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
+AXION_CAMERA_REAR_INFO := 13
+AXION_CAMERA_FRONT_INFO := 8
+AXION_MAINTAINER := Abdulwahab_(ai94iq)
+AXION_PROCESSOR := Qualcomm_Snapdragon_870
+
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildFingerprint=Xiaomi/pipa_global/pipa:13/RKQ1.211001.001/V816.0.7.0.UMZMIXM:user/release-keys
