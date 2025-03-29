0f4b246d63eef3f3c388ffcc6b39c47f9037bf95
Abdulwahab Isam <abdoi94.iq@gmail.com>
2025-03-30 00:46:36 +0300

    pipa: vendorsetup: switch sm8250-common branches
    Signed-off-by: Abdulwahab Isam <abdoi94.iq@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -15,9 +15,9 @@ clone_if_missing() {
 }
 
 # Git clones
-clone_if_missing "https://github.com/ai94iq/android_device_xiaomi_sm8250-common" "vic" "device/xiaomi/sm8250-common"
+clone_if_missing "https://github.com/ai94iq/android_device_xiaomi_sm8250-common" "axv-qpr2" "device/xiaomi/sm8250-common"
 clone_if_missing "https://github.com/ai94iq/android_kernel_xiaomi_sm8250" "axksu" "kernel/xiaomi/sm8250"
-clone_if_missing "https://github.com/ai94iq/proprietary_vendor_xiaomi_sm8250-common" "vic" "vendor/xiaomi/sm8250-common"
+clone_if_missing "https://github.com/ai94iq/proprietary_vendor_xiaomi_sm8250-common" "axv-qpr2" "vendor/xiaomi/sm8250-common"
 clone_if_missing "https://github.com/ai94iq/proprietary_vendor_xiaomi_pipa" "axv-qpr2" "vendor/xiaomi/pipa"
 
 # Additional LineageOS repositories
