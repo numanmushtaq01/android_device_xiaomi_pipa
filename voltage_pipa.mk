b8daa1e278e84376b8da46ce732a456c1258e584
Pranshu Sheoran <sheoranpranshu@gmail.com>
2025-05-22 17:39:31 +0530

    pipa: Bringup Voltage OS

--- voltage_pipa.mk
+++ voltage_pipa.mk
@@ -8,8 +8,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 
-# Inherit some common lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
+# Inherit some common voltage stuff.
+$(call inherit-product, vendor/voltage/config/common_full_tablet_wifionly.mk)
 
 # Inherit from pipa device
 $(call inherit-product, device/xiaomi/pipa/device.mk)
@@ -17,7 +17,7 @@ $(call inherit-product, device/xiaomi/pipa/device.mk)
 # Inherit keys
 $(call inherit-product, vendor/lineage-priv/keys/keys.mk)
 
-PRODUCT_NAME := lineage_pipa
+PRODUCT_NAME := voltage_pipa
 PRODUCT_DEVICE := pipa
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Xiaomi
@@ -27,18 +27,6 @@ PRODUCT_CHARACTERISTICS := tablet
 TARGET_SUPPORTS_QUICK_TAP := false
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
-# Axion specific
-AXION_CAMERA_REAR_INFO := 13
-AXION_CAMERA_FRONT_INFO := 8
-AXION_CPU_SMALL_CORES := 0,1,2,3
-AXION_CPU_BIG_CORES := 4,5,6,7
-AXION_MAINTAINER := Abdulwahab_(ai94iq)
-AXION_PROCESSOR := Qualcomm_Snapdragon_870
-TARGET_PREBUILT_BCR := false
-PRODUCT_NO_CAMERA := false
-TARGET_INCLUDE_MATLOG := true
-BYPASS_CHARGE_SUPPORTED := true
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildFingerprint=Xiaomi/pipa_global/pipa:13/RKQ1.211001.001/V816.0.7.0.UMZMIXM:user/release-keys
