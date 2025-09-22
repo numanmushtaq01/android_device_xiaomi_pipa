2ef145500499727dc615c71dae4a93b9fcb66c26
numanmushtaq01 <naveenmushtaq3@gmail.com>
2025-09-22 16:29:41 +0530

    pipa: Initialise for lineage

---
+++ lineage_pipa.mk
@@ -0,0 +1,32 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+
+# Inherit some common voltage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
+
+# Inherit from pipa device
+$(call inherit-product, device/xiaomi/pipa/device.mk)
+
+# Inherit keys
+$(call inherit-product, vendor/lineage-priv/keys/keys.mk)
+
+PRODUCT_NAME := lineage_pipa
+PRODUCT_DEVICE := pipa
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Pad 6
+
+PRODUCT_CHARACTERISTICS := tablet
+TARGET_SUPPORTS_QUICK_TAP := false
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BuildFingerprint=Xiaomi/pipa_global/pipa:13/RKQ1.211001.001/V816.0.7.0.UMZMIXM:user/release-keys
